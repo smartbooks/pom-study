74ec52aa6f28f539ce5cd4d378bf2373fd98a592
smartbooks <smartbooks@qq.com>
2018-03-07 15:30:10 +0800

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch master
     Your branch is up to date with 'origin/master'.
    
     Changes to be committed:
    	new file:   bi-etl/pom.xml
    	new file:   bi-etl/src/main/java/com/ljja/bi/etl/App.java
    	new file:   bi-etl/src/main/resources/conf.ini
    	new file:   bi-etl/src/main/scala/com/ljja/bi/etl/ScalaApp.scala
    	new file:   bi-profile/pom.xml
    	new file:   bi-report/pom.xml
    	new file:   bi-visual/pom.xml
    	new file:   pom.xml

---
+++ bi-etl/src/main/java/com/ljja/bi/etl/App.java
@@ -0,0 +1,7 @@
+package com.ljja.bi.etl;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("com.ljja.bi.etl.App");
+    }
+}
---
+++ bi-etl/src/main/scala/com/ljja/bi/etl/ScalaApp.scala
@@ -0,0 +1,9 @@
+package com.ljja.bi.etl
+
+object ScalaApp {
+
+  def main(args: Array[String]): Unit = {
+    println("com.ljja.bi.etl.ScalaApp")
+  }
+
+}
